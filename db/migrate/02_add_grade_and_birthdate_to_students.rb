fefdbf194341e7ead3e2582cf8c3c0a0abda5eba
Steven Chen <ysteven.chen2@gmail.com>
2018-05-25 18:50:41 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -3,6 +3,10 @@ class CreateStudents < ActiveRecord::Migration[5.1]
     add_column :students do |t|
       t.integer :grade
       t.string  :birthdate
+      
+      add_column :artists, :favorite_food, :string
+      
+      
     end
   end
  
